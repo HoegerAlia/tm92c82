4eefcef6445038d4e58ceb0e2d8459e6beb4ed4b
Lynch Deontae <114746732+HoegerAlia@users.noreply.github.com>
2022-11-18 10:48:40 +0000

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! -f UUID ]; then
-  UUID="ad806487-2d26-4636-98b6-ab85cc8521f7"
+  UUID="d19ab859-f648-4ed4-80a8-25fb3dd31ab2"
 fi
 
 # Set config.json
@@ -8,7 +8,7 @@ sed -i "s/PORT/$PORT/g" /etc/xray/config.json
 sed -i "s/UUID/$UUID/g" /etc/xray/config.json
 
 echo starting xray platform
-echo starting with UUID:$UUID
+echo starting 9ffdc8a7-d3f1-4176-862b-625662c5e4f9with UUID:$UUID
 echo listening at 0.0.0.0:$PORT
 
 exec "$@"
